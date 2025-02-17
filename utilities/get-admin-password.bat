7db0e656c90f56121e4fdd24788abd220c4c5b43
Hamlyn <49137816+hafio@users.noreply.github.com>
2025-02-17 18:33:12 +0800

    Create get-admin-password.bat

---
+++ utilities/get-admin-password.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set broker=ha-broker
+set namespace=solace
+
+if "%broker%"=="" (
+	kubectl.exe get eventbroker -n solace -o jsonpath="{.items[0].metadata.name}" > .tmp
+	set /p broker=<.tmp
+)
+
+kubectl.exe get eventbroker %broker% -n %namespace% -o jsonpath="{.status.broker.adminCredentialsSecret}" > .tmp
+set /p secname=<.tmp
+
+kubectl.exe get secret %secname% -n %namespace% -o jsonpath="{.data.username_admin_password}" > .tmp
+
+certutil.exe -f -decode .tmp .out >nul
+type .out
+
+if exist .tmp del .tmp
+if exist .out del .out
